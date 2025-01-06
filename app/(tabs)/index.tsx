450e911dbd2fda9392f363e0d8e3a327377e282b
Student-Ajit <ajitsingh86488@gmail.com>
2025-01-06 17:04:45 +0530

    Your commit message

--- app/(tabs)/index.tsx
+++ app/(tabs)/index.tsx
@@ -1,14 +1,13 @@
 import { View, StyleSheet } from 'react-native';
 import React from 'react';
-import HomePage from './HomePage';
-// import Device from './Device';
+ import HomePage from './HomePage';
 // import Login from './Login';
 // import ForgotPassword from './ForgotPassword'
-// import VegFood from './VegFood';
-// import NonVegFood from './NonVegFood';
-// import Add_To_Card from './Add_To_Card';
+ import VegFood from './VegFood';
+ import NonVegFood from './NonVegFood';
+ import Add_To_Card from './Add_To_Card';
 import { createNativeStackNavigator } from '@react-navigation/native-stack';
-// import BackgroundServices from "./BackgroundServices";
+// import BackgroundServices from "./BackgroundServices"; 
 
 const Stack = createNativeStackNavigator();
 
@@ -16,46 +15,42 @@ const Index = () => {
   return (
     <View style={styles.container}>
       <Stack.Navigator>
+      <Stack.Screen
+          name="HomePage"
+          component={HomePage}
+          options={{ headerShown: false }} 
+        /> 
         {/* <Stack.Screen
           name="Login"
           component={Login}
-          options={{ headerShown: false }}
+          options={{ headerShown: false }} 
         /> */}
         {/* <Stack.Screen
           name="ForgotPassword"
           component={ForgotPassword}
-          options={{ headerShown: false }}
-        /> */}
-        {/* <Stack.Screen
+          options={{ headerShown: false }} 
+        /> 
+        <Stack.Screen
           name="BackgroundServices"
           component={BackgroundServices}
-          options={{ headerShown: false }}
-        /> */}
+          options={{ headerShown: false }} 
+        />*/}
+       
         <Stack.Screen
-          name="HomePage"
-          component={HomePage}
-          options={{ headerShown: false }}
-        />
-         {/* <Stack.Screen
-          name="Device"
-          component={Device}
-          options={{ headerShown: false }}
-        /> */}
-        {/* <Stack.Screen
           name="VegFood"
           component={VegFood}
-          options={{ headerShown: false }}
-        />
+          options={{ headerShown: false }} 
+        />  
         <Stack.Screen
           name="NonVegFood"
           component={NonVegFood}
-          options={{ headerShown: false }}
-        />
-         <Stack.Screen
+          options={{ headerShown: false }} 
+        /> 
+        <Stack.Screen
           name="Add_To_Card"
           component={Add_To_Card}
-          options={{ headerShown: false }}
-        /> */}
+          options={{ headerShown: false }} 
+        />  
       </Stack.Navigator>
     </View>
   );
